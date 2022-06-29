89f3a1d3aedbf9a3852a67b6a281ae01a09b37b4
Ons <onsismailhajri21@gmail.com>
2022-06-29 05:03:30 +0100

    adding files
    add

--- src/app/dashbord/dashbord.page.ts
+++ src/app/dashbord/dashbord.page.ts
@@ -356,51 +356,6 @@ changeChart( event: any ) {
 addRandom( points: any ) : number {
   return Number(points) - Number( Math.floor((Math.random() * 100) + 1) );
 }
-/*showChart() {
-  var ctx = (<any>document.getElementById('yudhatp-chart')).getContext('2d');
-  var chart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-      labels: ["VB 6", "PHP", "Delphi", ".Net", "TypeScript"],
-      datasets: [{
-            label: "This is chart",
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            data: [this.getvoiyure, 5, 10, 25, 45],
-            borderWidth: 1
-      }]
-     }
-  });
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
